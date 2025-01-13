520e0be1c83f2213a673bac5c40aa0a4a81469ce
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-01-13 03:19:57 -0600

    Left Panel Initialize

--- script.js
+++ script.js
@@ -2,16 +2,44 @@ const title = document.getElementById("title");
 
 const description = document.getElementById("description");
 
+const leftPanelList = document.querySelector(".jellybean-list")
+
 axios.defaults.baseURL = 'https://jellybellywikiapi.onrender.com/api/Beans';
 
 
 
 async function initialize() {
-    const response = await axios("?flavorName=7up");
-    const data = response.data;
+    try{
+        const response = await axios.get("/");
+        const data = response.data;
 
-    console.log(data.imageUrl);
+        const jellybeans = data.items;
 
+        if(jellybeans.length < 1) {
+            console.log("problem");
+            return;
+        }
+
+
+        else
+            fillLeftPanel(jellybeans);
+
+    }
+    catch(err) {
+        console.log(err);
+    }
 };
+
+function fillLeftPanel(jellybeans) {
+    jellybeans.forEach((bean) => {
+        console.log(bean)
+        const listItem = document.createElement("li");
+        listItem.classList.add("list-group-item");
+        listItem.textContent = bean.flavorName;
+        listItem.dataset.beanId = bean.beanId;
+        leftPanelList.appendChild(listItem);
+    });
+}
+
 
 initialize();
