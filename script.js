50abf22924cf133d8963fe5c7432eb4d3b770a21
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-01-13 01:18:28 -0600

    Axios

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+const title = document.getElementById("title");
+
+const description = document.getElementById("description");
+
 axios.defaults.baseURL = 'https://jellybellywikiapi.onrender.com/api/Beans';
 
 
 
 async function initialize() {
-    const response = await axios();
+    const response = await axios("?flavorName=7up");
     const data = response.data;
 
-    console.log(data);
+    console.log(data.imageUrl);
 
 };
+
+initialize();
